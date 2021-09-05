0c3ca49a3075c20bcea824a5d63bf33aa0da8264
daniilkosogoiv <danil.kos@list.ru>
2021-09-05 22:43:47 +0300

    day 1

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,46 @@ testWebP(function(support) {
         document.querySelector('body').classList.add('no-webp');
     }
 });
+
+const reviews__wrap = document.querySelector('.reviews__wrap');
+const scroller = reviews__wrap.querySelector('.scroller');
+const nextBtn = reviews__wrap.querySelector('.btn.next');
+const prevBtn = reviews__wrap.querySelector('.btn.prev');
+const itemWidth = reviews__wrap.querySelector('.item').clientWidth;
+
+nextBtn.addEventListener('click', scrollToNextItem);
+prevBtn.addEventListener('click', scrollToPrevItem);
+
+function scrollToNextItem() {
+    if (scroller.scrollLeft < (scroller.scrollWidth - itemWidth))
+
+        scroller.scrollBy({
+        left: itemWidth,
+        top: 0,
+        behavior: 'smooth'
+    });
+    else
+
+        scroller.scrollTo({
+        left: 0,
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+function scrollToPrevItem() {
+    if (scroller.scrollLeft != 0)
+
+        scroller.scrollBy({
+        left: -itemWidth,
+        top: 0,
+        behavior: 'smooth'
+    });
+    else
+
+        scroller.scrollTo({
+        left: scroller.scrollWidth,
+        top: 0,
+        behavior: 'smooth'
+    });
+}
